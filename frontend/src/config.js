d10f5c8a1f9b9f992ec386f256c8c2560d427dd3
yasser <yasserfakir3@gmail.com>
2025-07-10 17:54:03 +0100

    d

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: 'portfolio-creator-production.up.railway.app',
+  API_URL: 'https://portfolio-creator-production.up.railway.app',
   isDevelopment: process.env.NODE_ENV === 'development',
   isProduction: process.env.NODE_ENV === 'production'
 };
