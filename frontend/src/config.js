ba70bf37d42e3638f1a7b4a73efa1281d8a78fec
yasser <yasserfakir3@gmail.com>
2025-07-10 17:15:23 +0100

    test1

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  API_URL: 'portfolio-creator-production.up.railway.app',
   isDevelopment: process.env.NODE_ENV === 'development',
   isProduction: process.env.NODE_ENV === 'production'
 };
